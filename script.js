d6adc24803939638cecb91e6170d8e09ddd07873
camposCode <info@josecampos.io>
2022-04-26 08:02:37 +0200

    mute/unmute done

--- script.js
+++ script.js
@@ -53,6 +53,7 @@ function setProgress(e){
 }
 
 // Volume Controls --------------------------- //
+let lastVolume = 1;
 // Volume Bar
 function changeVolume(e){
     let volume = e.offsetX / volumeRange.offsetWidth;;
@@ -75,6 +76,24 @@ function changeVolume(e){
     }else if(volume === 0){
         volumeIcon.classList.add('fas', 'fa-volume-off');
     }
+    lastVolume = volume;
+}
+
+// Mute/unmute
+function toggleMute(){
+    volumeIcon.className = '';
+    if(video.volume){
+        lastVolume = video.volume;
+        video.volume = 0;
+        volumeBar.style.width = 0;
+        volumeIcon.classList.add('fas', 'fa-volume-mute');
+        volumeIcon.setAttribute('title', 'Unmute');
+    }else{
+        video.volume = lastVolume;
+        volumeBar.style.width = `${lastVolume * 100}%`;
+        volumeIcon.classList.add('fas', 'fa-volume-up');
+        volumeIcon.setAttribute('title', 'Mute');
+    }
 }
 
 // Change Playback Speed -------------------- //
@@ -91,3 +110,4 @@ video.addEventListener('timeupdate', updateProgress);
 video.addEventListener('canplay', updateProgress);
 progressRange.addEventListener('click', setProgress);
 volumeRange.addEventListener('click', changeVolume);
+volumeIcon.addEventListener('click', toggleMute);
